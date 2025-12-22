ccb88e6f21c9f4c594dc47d9ee79f201c757e5ad
theducdev <theduc.ng@gmail.com>
2025-12-23 01:11:06 +0700

    Refactor for Vercel Serverless Functions

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,10 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
